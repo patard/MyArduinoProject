387d15c4b45442681c6ca60852fb1c44256bb05d
Patrick TATARD <patrick.tatard@gmail.com>
2016-06-27 21:27:43 +0200

    Add files via upload

---
+++ src/testIsis.ino
@@ -0,0 +1,17 @@
+#include <Isis.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Isis.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if ( Isis.hasNewData() )
+  {
+    Isis.decodeMessage();
+  }
+}
+
+
