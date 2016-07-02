122c59f4ba2feb20fde1e9e4d77c04c1a21faf0f
Patrick TATARD <Patrick TATARD>
2016-07-02 18:36:59 +0200

    testIris

--- src/testIris/testIris.ino
+++ src/testIris/testIris.ino
@@ -1,16 +1,46 @@
+/*
+   The aim of this sketch is to test the Iris library.
+   It must be downloaded in an Arduino Nano or Uno.
+   The ArduinoUnit library is needed (https://github.com/mmurdoch/arduinounit)
+
+   Two arduino needed :
+   - one tester on which this sketch is downloaded
+   - one Arduino under test (AUT) where the Iris main sketch is downloaded
+
+   They are connected via I2C:
+
+   Arduino tester pin number          Arduino under test pin number
+            A4  --------------------------------  A4
+            A5  --------------------------------  A5
+            GND -------------------------------- GND
+            D13 -------------------------------- D13 (test digitalWrite)
+            D12 -------------------------------- D12 (test digitalRead)
+            A0  -------- R1 = 1kOhm ------------ D3  (test analogWrite)
+                              |
+                              |----------------- A0  (test analogRead)
+                              |
+                         C1 = 470 uF  (make a RC circuit to filter PWM)
+                              |
+                              |
+                             GND
+
+
+
+
+   For an explanation of the RC circuit (for example) :
+   cf http://www.instructables.com/id/Arduino-RC-Circuit-PWM-to-analog-DC/step6/Arduino-RC-circuit/
+
+*/
+
+
 #include <ArduinoUnit.h>
 #include <Wire.h>
 
-#include <Iris.h>
+#include <IrisArduino.h>
 
-#define I2C_SLAVE_ADDR 0x05
+#define I2C_SLAVE_ADDR 0x05 // address of the AUT on the I2C
 
-test(ok)
-{
-  int x = 3;
-  int y = 3;
-  assertEqual(x, y);
-}
+int testNumber;
 
 /*
   test(bad)
@@ -21,146 +51,222 @@ test(ok)
   // assertEqual(x, y);
   }*/
 
-/*test(led13)
+
+test(analRead)
 {
-  byte msgId;
-  byte data;
+  Serial.println(F("ENTRY analogRead"));
+
+  byte reading[ANALOG_READ_VALUE_MSG_SIZE];
+ // int pinNumberMaster = A0;
+  int pinNumberAutWrite = 3; //Arduino under test => write PWM
+  int pinNumberAutRead = 0; //Arduino under test A0
+  // to reuse the RC circuit
+  int valueToSet = 125;
+  int valueReadOnMaster;
+  int valueReadOnAut;
+
+  // send DEBUG Msg
+  Iris.debugMsgReq(testNumber);
+
+
+  // ---- Write analog value on pin on AUT
+  Serial.println( F("Write analog value on AUT"));
+  Iris.analogWriteReq(pinNumberAutWrite, valueToSet);
+  delay(1000); // let RC capacity to charge
+
+  // -- read on Analog input on master // TODO compare with analogReadReq
+  valueReadOnMaster = analogRead(A0);
+  Serial.println(valueReadOnMaster);
+  // Serial.println(map(valueRead, 0, 1023, 0, 255));
+  Serial.println("--");
+
+  delay(100);
+  // -- request analog read on AUT
+  Iris.analogReadReq(pinNumberAutRead);
+
+  
+  // ---- check data returned
+  Serial.println( F("Check value read on AUT"));
+  if (ANALOG_READ_VALUE_MSG_SIZE <= Wire.available()) { // if two bytes were received
+    reading[0] = Wire.read();  // receive high byte (overwrites previous reading) it is the msgId
+    assertEqual(reading[0], ANALOG_READ_VALUE_MSG_ID);
+    reading[1] = Wire.read();
+    reading[2] = Wire.read();
+ 
+    int pinNumber = 0;
+    
+    Serial.println(reading[1]);
+    Serial.println(reading[2]);
+     IrisClass::decodeAnalogReadValue(&reading[1], &pinNumber, &valueReadOnAut);
+    /*  Serial.println(pinNumber);
+      // assertEqual(pinNumber, pinNumberAutRead);
+      Serial.println(valueRead);
+      // assertEqual(valueRead, valueToSet);*/
+  }
+
+
+  assertEqual(1, 1);
+
+  Serial.println(F("EXIT analogRead"));
+  Serial.println(F("---------------------------"));
+  testNumber++;
+}
+
+/*
+  test(analWrite) // TODO assert
+  {
+  Serial.println(F("ENTRY analogWrite"));
+
+  int pinNumberMaster = A0;
+  int pinNumberAut = 3; //Arduino under test
+  int valueToSet = 125;
+  int valueRead;
+
+  // send DEBUG Msg
+  Iris.debugMsgReq(testNumber);
+
+  // ---- Write analog value on pin on AUT
+  Serial.println( F("Write analog value on AUT"));
+  Iris.analogWriteReq(pinNumberAut, valueToSet);
+  delay(1000); // let RC capacity to charge
+
+  // -- read on Analog input
+  valueRead = analogRead(pinNumberMaster);
+  Serial.println(valueRead);
+  // Serial.println(map(valueRead, 0, 1023, 0, 255));
+  Serial.println("--");
+
+  valueToSet = 62;
+  Iris.analogWriteReq(pinNumberAut, valueToSet);
+  delay(1000);
+  valueRead = analogRead(pinNumberMaster);
+  Serial.println(valueRead);
+  //Serial.println(map(valueRead, 0, 1023, 0, 255));
+
+  assertEqual(1, 1);
+
+  Serial.println(F("EXIT analogWrite"));
+  Serial.println("---------------------------");
+  testNumber++;
+  }*/
+
+/*
+  test(digitWrite)
+  {
   int value;
-  // set pin 13 to OUTPUT
-  Wire.beginTransmission(I2C_SLAVE_ADDR); // transmit to device
-  msgId = 1;
-  data = 53; // 0x35
-  Wire.write(msgId);
-  Wire.write(data); // sends one byte
-  Wire.endTransmission(); // stop transmitting
-  //delay(100); // the Isis is treating message
-  // set pin 13 to LOW
-  Wire.beginTransmission(I2C_SLAVE_ADDR); // transmit to device
-  msgId = 3;
-  data = 52; // 0x34
-  Wire.write(msgId);
-  Wire.write(data); // sends one byte
-  Wire.endTransmission(); // stop transmitting
-  // check result
+
+  Serial.println(F("ENTRY digitalWrite"));
+
+  int pinNumberMaster = 13;
+  int pinNumberAut = 13; //Arduino under test
+
+  // send DEBUG Msg
+  Iris.debugMsgReq(testNumber);
+
+  // ---- Set pin on AUT to OUTPUT MODE
+  Serial.println( F("Set pin on AUT to OUTPUT MODE"));
+  Iris.pinModeReq(pinNumberAut, OUTPUT_PIN_MODE);
+
+  // -- set pin on AUT to LOW
+  Serial.println( F("Set pin on AUT to LOW"));
+  Iris.digitalWriteReq(pinNumberAut, LOW);
+  // -- check result
   delay(100);
-  value = digitalRead(13);
-  Serial.print("testLed13 low ");
-  Serial.println (value);
+  value = digitalRead(pinNumberMaster);
   assertEqual(value, LOW);
-  // set pin 13 to HIGH
-  Wire.beginTransmission(I2C_SLAVE_ADDR); // transmit to device
-  msgId = 3;
-  data = 53; // 0x35
-  Wire.write(msgId);
-  Wire.write(data); // sends one byte
-  Wire.endTransmission(); // stop transmitting
-  // check result
+
+  // -- set pin on AUT to HIGH
+  Serial.println( F("Set pin on AUT to HIGH"));
+  Iris.digitalWriteReq(pinNumberAut, HIGH);
+  // --check result
   delay(100);
-  value = digitalRead(13);
-  Serial.print("testLed13 high ");
-  Serial.println (value);
+  value = digitalRead(pinNumberMaster);
   assertEqual(value, HIGH);
-}*/
+
+  Serial.println(F("EXIT digitalWrite"));
+  Serial.println("---------------------------");
+  testNumber++;
+  }
 
 
-test(digitRead)
-{
-  byte msgId;
-  byte data;
+  test(digitRead)
+  {
+  byte reading[DIGITAL_READ_VALUE_MSG_SIZE];
   bool valueSet;
+
+  Serial.println(F("ENTRY digitalRead"));
 
   int pinNumberMaster = 12;
   int pinNumberAut = 12; //Arduino under test
 
-// set pin 7 to INPUT
-  Wire.beginTransmission(I2C_SLAVE_ADDR); // transmit to device
-  msgId = PIN_MODE_MSG_ID;
-  data = (pinNumberAut << 2) |  INPUT_PIN_MODE ; // ne pas mettre en pullup
-  Wire.write(msgId);
-  Wire.write(data); // sends one byte
-  Wire.endTransmission(); // stop transmitting
+  // send DEBUG Msg
+  Iris.debugMsgReq(testNumber);
 
-  delay(100); 
+  // ---- Set pin on AUT to INPUT MODE
+  // do NOT set INPUT_PULLUP
+  Serial.println( F("Set pin on AUT to INPUT MODE"));
+  Iris.pinModeReq(pinNumberAut, INPUT_PIN_MODE);
 
-  // ------------- set pinNumberMaster to High on master
+  delay(100);
+
+  // ---- set pinNumberMaster to High on master
+  Serial.println( F("Set pin on Tester to HIGH"));
   valueSet = HIGH;
   digitalWrite(pinNumberMaster, valueSet);
 
-  // Digital_read_request
-  Wire.beginTransmission(I2C_SLAVE_ADDR); // transmit to device
-  // encode msg digitalRead on pinNumber
-  msgId = DIGITAL_READ_MSG_ID;
-  data = pinNumberAut; 
-  Wire.write(msgId);
-  Wire.write(data); // sends one byte
-  Wire.endTransmission(); // stop transmitting
-  
-  // step 2: wait for readings to happen
-  delay(70);                   // datasheet suggests at least 65 milliseconds ....
-  
-  // step 4: request reading from sensor
-  Wire.requestFrom(I2C_SLAVE_ADDR, 2);    // request 2 bytes from slave device
-  // step 5: receive reading from sensor
-  byte reading[2];
-  if (2 <= Wire.available()) { // if two bytes were received
+  // ---- Digital_read_request
+  Serial.println( F("Request digital read on AUT"));
+  Iris.digitalReadReq(pinNumberAut);
+
+  // ---- check data returned
+  Serial.println( F("Check value read on AUT"));
+  if (DIGITAL_READ_VALUE_MSG_SIZE <= Wire.available()) { // if two bytes were received
     reading[0] = Wire.read();  // receive high byte (overwrites previous reading) it is the msgId
-    //check msgId == DIGITAL_READ_VALUE_MSG_ID
     assertEqual(reading[0], DIGITAL_READ_VALUE_MSG_ID);
     reading[1] = Wire.read();
     // decode digitalRead
     int pinNumber = 0;
-    int valueRead;
-    IrisClass::decodeDigitalRead(reading[1], &pinNumber, &valueRead);
+    byte valueRead;
+    IrisClass::decodeDigitalReadValue(&reading[1], &pinNumber, &valueRead);
     assertEqual(pinNumber, pinNumberAut);
     assertEqual(valueRead, valueSet);
   }
 
-
-// ------------- set pinNumberMaster to Low on master
-  valueSet = LOW;
+  // ------------- set pinNumberMaster to Low on master
+  Serial.println( F("Set pin on Tester to HIGH"));
+  valueSet = HIGH;
   digitalWrite(pinNumberMaster, valueSet);
 
-  // Digital_read_request
-  Wire.beginTransmission(I2C_SLAVE_ADDR); // transmit to device
-  // encode msg digitalRead on pinNumber
-  msgId = DIGITAL_READ_MSG_ID;
-  data = pinNumberAut; 
-  Wire.write(msgId);
-  Wire.write(data); // sends one byte
-  Wire.endTransmission(); // stop transmitting
-  
-  // step 2: wait for readings to happen
-  delay(70);                   // datasheet suggests at least 65 milliseconds ....
-  
-  // step 4: request reading from sensor
-  Wire.requestFrom(I2C_SLAVE_ADDR, 2);    // request 2 bytes from slave device
-  // step 5: receive reading from sensor
-  //byte reading[2];
-  if (2 <= Wire.available()) { // if two bytes were received
+  // ---- Digital_read_request
+  Serial.println( F("Request digital read on AUT"));
+  Iris.digitalReadReq(pinNumberAut);
+
+  // ---- check data returned
+  Serial.println( F("Check value read on AUT"));
+  if (DIGITAL_READ_VALUE_MSG_SIZE <= Wire.available()) { // if two bytes were received
     reading[0] = Wire.read();  // receive high byte (overwrites previous reading) it is the msgId
-    //check msgId == DIGITAL_READ_VALUE_MSG_ID
     assertEqual(reading[0], DIGITAL_READ_VALUE_MSG_ID);
     reading[1] = Wire.read();
     // decode digitalRead
     int pinNumber = 0;
-    int valueRead;
-    IrisClass::decodeDigitalRead(reading[1], &pinNumber, &valueRead);
+    byte valueRead;
+    IrisClass::decodeDigitalReadValue(&reading[1], &pinNumber, &valueRead);
     assertEqual(pinNumber, pinNumberAut);
-    Serial.println(valueSet);
-    Serial.println(valueRead);
     assertEqual(valueRead, valueSet);
   }
 
-  
-}
+  Serial.println(F("EXIT digitalRead"));
+  Serial.println("---------------------------");
+  testNumber++;
+  }*/
 
 
 void setup()
 {
-  pinMode(13, INPUT_PULLUP);
-  Wire.begin(); // join i2c bus (address optional for master)
-  Serial.begin(9600);
-  Serial.println("---------------------------");
+  testNumber = 0;
+  // pinMode(A0, INPUT);
+  Iris.beginMaster(I2C_SLAVE_ADDR);
+  Serial.println(F("---------------------------"));
 }
 
 void loop()
